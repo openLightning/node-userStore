54bed88c486d8193710b498313c0e0cf815ce442
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-03-14 18:48:34 +0000

    Now with many improvements...

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var express = require('express');
 var storage = require('node-persist');
 var app = express();
 
-app.post('/set/', function(req, res){
+/* Start User Functions */
+function existingUser(username){
+  if(storage.getItem(username)!=undefined){
+		return true;
+	} else{
+		return false;
+	}
+}
+       
+function newUser(username, password){
+	var user = {
+		name:username,
+		pass:password,
+		data:{
+			
+		}
+	}
+}
+
+/* End User Functions*/
+app.post('/set/', function (req, res){
   storage.setItem(req.param('name'),req.param('data'));
   res.send('Done');
 });
-app.post('/get/', function(req, res){
+app.post('/get/', function (req, res){
   res.send(storage.getItem(req.param('name')));
 });
 
